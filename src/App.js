32bbc9baade96b53c0460f4db30a332f5ed4b7d9
louiskgkim <lkgk31@gmail.com>
2023-04-02 11:41:59 -0700

    adding app.js, and resume.js and removing portfolio.json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-// TODO: Add a comment explaining what this import statement is doing
-// This import statement is importing a component named "HelloReact" from 
-// the "./components/HelloReact" file path. 
-import HelloReact from './components/about';
+import Header from './components/header';
+import Footer from './components/footer';
 
-// TODO: Add a comment explaining the purpose of the App component
-// The purpose of the App component is to act as a container or 
-// wrapper for other components in the application. 
-// App component is a functional component that returns the 
-// <HelloReact /> component. This means that when the App component 
-// is rendered to the DOM, it will render the <HelloReact /> component 
-// as a child component.
 function App() {
-  return <HelloReact />;
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
