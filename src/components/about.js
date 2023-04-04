5f0b3225cbbebca782c129d0d52ea55050cf9a9f
louiskgkim <lkgk31@gmail.com>
2023-04-03 19:57:43 -0700

    adding finished portfolio, navigation and about js files

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,40 @@ import React from "react";
 
 function About() {
   return (
-    
+    <div>
+      <p className="content is-large">Full Stack/Junior Developer Developer</p>
+      <hr />
+      <img
+        className="pic"
+        src={process.env.PUBLIC_URL + "/images/profile.jpg"}
+        alt="Louis Kim Photo"
+      />
+      <p className="content is-italic mt-5">
+        Hello my name is Louis and I'm currently enrolled in Web Developer
+        Coding bootcamp through University of Washington, to be certified in May
+        2023. I'm an ambitious candidate who has passion for problem solving and
+        optimizing business processes from a customer’s perspective and needs.
+      </p>
+      <p className="content">
+        Currently, I work at a small real estate tech company as a CSR II and
+        I'm on the course of becoming an Application Support Engineer. SQL is
+        the main thing that I'm working with in pgAdmin but I'm also been
+        looking into coding. I found this course and decided to take it in order
+        to help with my career growth/change. I'm excited to continue working
+        and learning more about coding in general. I'm glad I had a small
+        general background in college which has already been beneficial to me in
+        the Prework and first class.
+        <br></br>
+        By the end of the year when I'm promoted to an Application Support
+        Engineer I hope to be able to hone more skills in SQL and get more
+        familiar with tools like pgAdmin and Kibana. I do wish to be able to
+        learn a lot from this coding bootcamp so that I can further my career
+        path by becoming a junior software engineer at my current company if the
+        chance arises. This challenge was a very fun one to do with all the
+        skills I have so far learned throughout the weeks and I can't wait to
+        continue to learn more.
+      </p>
+    </div>
   );
 }
 
---
+++ src/components/portfolio.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Project from "./project";
+
+const projects = [
+  {
+    id: 0,
+    title: "CheersCheck",
+    languages: "html, css, js, MySQL",
+    packages:
+      "bcrypt, Express.js, connect-session-sequelize, dotenv, express-handlebars, express-session, jshint, masonry-layout, mysql2, nodemon and sequelize",
+    image: "/images/cheers-check.png",
+    description:
+      "A web application where alcohol enthusiasts can keep track of what's in their liquor cabinet. Users can make comments on other user's inventory as well. Using Handlebars.js as the templating language, Sequelize as the ORM, and express-session npm package for authentication.",
+    repo: "https://github.com/louiskgkim/CheersCheckCopy",
+  },
+  {
+    id: 1,
+    title: "Professional-README-Generator",
+    languages: "js",
+    packages:
+      "node.js, inquirer",
+    image: "/images/generated-readme.png",
+    description:
+      "Based on specific questions answered by the user, a complete professional README file will be created based on those answers.",
+    repo: "https://github.com/louiskgkim/Professional-README-Generator",
+  },
+  {
+    id: 2,
+    title: "Employee Tracker",
+    languages: "js",
+    packages:
+      "node.js, inquirer, mysql2",
+    image: "/images/employee-tracker.png",
+    description:
+      "Created an interface so that it is easy for non-developers to easily view and interact with information within a database. These interfaces are called content management systems (CMS). My assignment this week is to build a command-line application from scratch to manage a company's employee database, using Node.js, Inquirer, and MySQL.",
+    repo: "https://github.com/louiskgkim/Employee-Tracker",
+  },
+  {
+    id: 3,
+    title: "Fitness Planner",
+    languages: "html, css, js",
+    image: "/images//fitness-planner.png",
+    packages: "none",
+    description:
+      "A web application that helps users decide what workouts to do based on the weather on a searched city.",
+    repo: "https://github.com/louiskgkim/Fitness-Planner",
+  },
+  {
+    id: 4,
+    title: "Weather Dashboard",
+    languages: "html, css, js",
+    packages: "none",
+    image: "/images/weather-dashboard.png",
+    description:
+      "This weather dashboard will show today's current weather data and the next five days weather forecast of the searched city.",
+    repo: "https://github.com/louiskgkim/Weather-Dashboard",
+  },
+];
+
+function Portfolio() {
+  return (
+    <div>
+      <p className="content is-medium">Portfolio</p>
+      <hr />
+
+      <Project projects={projects} />
+    </div>
+  );
+}
+
+export default Portfolio;
