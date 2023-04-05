dff25508a3bc1b56827071e38e9fd83bd07784ba
louiskgkim <lkgk31@gmail.com>
2023-04-04 19:04:26 -0700

    finished contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -35,8 +35,62 @@ function Contact() {
     e.preventDefault();
   }
 
-  return {
+  return (
     <div>
-    
-  }
-  export default Contact;
+      <p className="content is-medium">Contact Me</p>
+      <hr />
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div className="field">
+          <label className="label" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="input"
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div className="field">
+          <label className="label" htmlFor="email">
+            Email Address
+          </label>
+          <input
+            className="input"
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div className="field">
+          <label className="label" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="textarea"
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="is-danger">{errorMessage}</p>
+          </div>
+        )}
+        <button
+          className="button is-medium is-primary is-fullwidth"
+          data-testid="button"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
